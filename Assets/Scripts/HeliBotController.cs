2c592ebe0380ce65a946fb71813bd32008cecbb3
Russell Strauss <russellstrauss@gmail.com>
2020-06-27 00:00:23 -0400

    Add basic helicopter bot

---
+++ Assets/Scripts/HeliBotController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeliBotController : MonoBehaviour
+{
+	private GameObject propeller;
+	private float rotation;
+	private float timer = 0.0f;
+	private float baseSpeed = 120f;
+	private float maxSpeed = 3000f;
+	private float health = 100f;
+	
+	void Start() {
+		propeller = GameObject.Find("Propeller");
+	}
+	
+	void Update() {
+		timer += Time.deltaTime;
+		if (rotation < maxSpeed) rotation = (float)Math.Log(timer, 1.1) * baseSpeed;
+		Debug.Log(rotation);
+		propeller.transform.Rotate(new Vector3(0, rotation, 0) * Time.deltaTime);
+	}
+}
