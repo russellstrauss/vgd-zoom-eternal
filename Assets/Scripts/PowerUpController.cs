01a452c2e175ff2e898e5a6d54b4c48bc5a9468f
Austin Jiang <valour8@hotmail.com>
2020-06-29 09:35:20 -0400

    add powerup

--- Assets/Scripts/PowerUpController.cs
+++ Assets/Scripts/PowerUpController.cs
@@ -9,12 +9,30 @@ public class PowerUpController : MonoBehaviour
 	public GameObject pickupEffect;
 	// public float multiplier = 1.4f;
 	public float duration = 2;
+	// public Vector3 RotateAmount= new Vector3(0.0f, 1.0f, 0.0f);
+	public float spinForce = 200.0f;
+
+
+	void Start()
+	{
+		// var rb = GetComponent<Rigidbody>();
+        // rb.angularVelocity = Random.insideUnitSphere;
+	}
+	void Update()
+	{
+		// transform.Rotate(RotateAmount * Time.deltaTime);
+		transform.Rotate(0, spinForce * Time.deltaTime, 0);
+		// transform.rotate(Vector3.right, rotateSpeed * Time.deltaTime, Space.World);
+	}
 
     void OnTriggerEnter(Collider other)
     {
     	if (other.CompareTag("Player")){
     		// Pickup(other);
     		StartCoroutine(Pickup(other));
+			// FindObjectOfType<AudioManager>().Play("ting");
+			FindObjectOfType<AudioManager>().Play("crash");
+			
     	}
 
 
