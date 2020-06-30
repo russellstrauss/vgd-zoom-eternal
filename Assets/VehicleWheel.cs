f528be4e36fbf58b3b40a30cd1082d1cf22f58cc
jeremydiba <jeremydibattista@gmail.com>
2020-06-29 20:37:47 -0400

    animation works but pretty bad

---
+++ Assets/VehicleWheel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleWheel : MonoBehaviour
+{
+    public WheelCollider targetWheel;
+    private Vector3 wheelPosition = new Vector3();
+    private Quaternion wheelRotation = new Quaternion();
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void Update()
+    {
+        // targetWheel.GetWorldPose(out wheelPosition, out wheelRotation);
+        // transform.position = wheelPosition;
+        // transform.rotation = wheelRotation * Quaternion.Euler(90, 0, 0);;
+    }
+}
