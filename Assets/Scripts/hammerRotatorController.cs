13e9a80c66951613249efa7cdbee3c763b81fd9a
Austin Jiang <valour8@hotmail.com>
2020-07-16 23:18:57 -0400

    add hammer damage to the function

--- Assets/Scripts/hammerRotatorController.cs
+++ Assets/Scripts/hammerRotatorController.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class hammerRotatorController : MonoBehaviour
 {
     // Start is called before the first frame update
-    private float origionZ;
+    public GameObject pickupEffect;
+	// public float multiplier = 1.4f;
+	public float duration = 2;
+	private GameObject player;
+	private Rigidbody playerRb;
+	private HeliBotController heliBotController;
     private Quaternion targetRotation;
     public int count = 0;
     // Use this for initialization
-    public Vector3 RotateSpeed;
     public float speed = 0.1f;
     public Quaternion originalRotationValue;
     private GameObject m_cube;
     void Start () {
-        // origionZ = transform.rotation.z;
         originalRotationValue = transform.rotation;
         m_cube = GameObject.FindWithTag("hammer_trigger");
+        player = GameObject.FindWithTag("Player");
+		playerRb = player.GetComponent<Rigidbody>();
+        heliBotController = player.GetComponent<HeliBotController>();
     }
     
     // Update is called once per frame
@@ -26,12 +32,6 @@ public class hammerRotatorController : MonoBehaviour
         // transform.Rotate(new Vector3(0, 0, 1));
         // targetRotation = Quaternion.Euler(0,180,RotateAngle*count+origionZ) * Quaternion.identity;
         // transform.RotateAround(GameObject.FindWithTag("hammer").transform.position + new Vector3(0, 0, 5) , new Vector3(0, 0, 1), 1 );
-        // transform.Rotate (Vector3.up * 50 * Time.deltaTime, Space.Self);
-        // currentTime += Time.deltaTime;
-        // if (currentTime < 7f)
-        // {
-        //     transform.localEulerAngles += (RotateSpeed * Time.deltaTime * speed);
-        // }
         count++;
         
         if (m_cube.GetComponent<hammerTriggerController>().EnteredTrigger)
@@ -48,5 +48,34 @@ public class hammerRotatorController : MonoBehaviour
             // print("GameObject.FindWithTag().transform::::::"+GameObject.FindWithTag("hammer").transform.rotation);
         }
 
+    }
+    void OnCollisionEnter(Collision collision){
+		
+		if (collision.collider.CompareTag("playerCollider")) {
+			Vector3 contactNormal = collision.contacts[0].normal;
+			playerRb.AddForce(new Vector3(0, 1, 0) * 10000, ForceMode.Impulse);
+			FindObjectOfType<AudioManager>().Play("crash");
+			StartCoroutine(Damage());
+		}
+	}
+
+    IEnumerator Damage()
+    {
+    	GameObject clone = Instantiate(pickupEffect, transform.position, transform.rotation);
+    	
+    	// TODO add more healthy to the player
+		heliBotController.SubtractHealth(100);
+
+    	// GetComponent<MeshRenderer>().enabled = false;
+    	// GetComponent<Collider>().enabled = false;
+
+    	//remove the effect from theplayer
+    	yield return new WaitForSeconds(duration);
+    	//wait x amount of seconds
+
+    	// remove power up object
+    	ParticleSystem.MainModule particle = clone.GetComponent<ParticleSystem>().main;
+    	Destroy(clone, duration);
+    	// Destroy(gameObject);
     }
 }
