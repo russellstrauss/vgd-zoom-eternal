6b23cec17546bdda57f12cb971fd6d201352ce0f
Russell Strauss <russellstrauss@gmail.com>
2020-07-25 00:32:06 -0400

    fix compilatioon error

---
+++ Assets/Scripts/SawBladeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+using UnityEngine.InputSystem;
+
+public class SawBladeController : MonoBehaviour {
+	
+	[HideInInspector]
+	public bool isColliding = false;
+	
+	Vector3 rotation = new Vector3(0, 800, 0);
+	float sawOscillationDistance = .01f;
+	Vector3 oscillationDirection;
+	Vector3 oscillation;
+	float randomSeed;
+	GameObject[] blades;
+	GameObject player;
+	Rigidbody[] sawRigidbodies;
+	
+	Vector3 displacement = new Vector3(0, 4f, 0);
+	float sawSpeed = 5f;
+	Vector3[] bladeStartingPositions;
+	float sawTimer = 0f;
+	float sawActiveTime = 8f;
+	
+	// Test vars
+	int count = 0;
+	
+	void Start() {
+		player = GameObject.FindWithTag("Player");
+		blades = GameObject.FindGameObjectsWithTag("hazard");
+		sawRigidbodies = new Rigidbody[blades.Length];
+		
+		bladeStartingPositions = new Vector3[blades.Length];
+		for (int i = 0; i < blades.Length; i++) {
+			
+			sawRigidbodies[i] = blades[i].GetComponent<Rigidbody>();
+			
+			bladeStartingPositions[i] = blades[i].transform.position;
+			
+			Vector3 targetPosition = bladeStartingPositions[i] + displacement;
+			// blades[i].transform.position = targetPosition;
+			Debug.DrawLine(bladeStartingPositions[i], targetPosition, Color.red);
+		}
+	}
+
+	void Update() {
+		
+		sawTimer += Time.deltaTime;
+
+		for (int i = 0; i < blades.Length; i++) {
+			
+			blades[i].transform.Rotate(rotation * Time.deltaTime);
+			
+			Debug.DrawRay(bladeStartingPositions[i], displacement, Color.green);
+			Debug.DrawRay(bladeStartingPositions[i], new Vector3(0, .2f, 0), Color.red);
+			Debug.DrawRay(bladeStartingPositions[i] + displacement, new Vector3(0, .2f, 0), Color.red);
+			
+			if (sawTimer < sawActiveTime / 2) {
+				Vector3 targetPosition = bladeStartingPositions[i] + displacement;
+				// Doesn't move up when timer is reset my player collision. Why?
+				blades[i].transform.position = Vector3.MoveTowards(blades[i].transform.position, targetPosition, sawSpeed * Time.deltaTime);
+				// Debug.Log("saw moving up, sawTimer=" + sawTimer + ", count=" + count);
+				count++;
+			}
+			else if (sawTimer > sawActiveTime / 2) {
+				count = 0;
+				blades[i].transform.position = Vector3.MoveTowards(blades[i].transform.position, bladeStartingPositions[i], sawSpeed * Time.deltaTime);
+			}
+			if (sawTimer > sawActiveTime) sawTimer = 0;
+		}
+	}
+	
+	public void TriggerAttack(Collider otherCollision) {
+		sawTimer = 0;
+		count = 0;
+		isColliding = true;
+		// FindObjectOfType<AudioManager>().Play("crash");
+	}
+	
+	public void ExitAttack() {
+		isColliding = false;
+	}
+}
